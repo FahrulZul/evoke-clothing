a5d48ff0a45af94a3723ecd16b21436e09616d39
FahrulZul <fahrulzul96@gmail.com>
2022-08-18 10:22:25 +0800

    Update sign-in component as well as firebase utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,9 +29,25 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = await doc(auth, "users", userAuth.uid);
+    const userDocRef = await doc(db, "users", userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
     console.log(userSnapshot);
     console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+            });
+        } catch (error) {
+            console.log("Error creating user", error.message);
+        }
+    }
+
+    return userDocRef;
 };
