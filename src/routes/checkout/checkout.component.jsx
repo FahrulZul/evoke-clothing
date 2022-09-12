477e59da5de55b250983bb8be9f8d94df5ac132b
FahrulZul <fahrulzul96@gmail.com>
2022-09-12 14:33:07 +0800

    Edit currency on checkout

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -33,7 +33,7 @@ const Checkout = () => {
             {cartItems.map((cartItem) => (
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />
             ))}
-            <div className="total">TOTAL: ${cartTotal}</div>
+            <div className="total">TOTAL: RM {cartTotal}</div>
 
             <PaymentForm />
         </div>
